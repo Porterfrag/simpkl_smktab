cc60bc9abaef9dad890d9439c118b35071f6b9cf
Ridho Wahyu Darmawan <ridhozedd@gmail.com>
2026-01-05 22:50:08 +0800

    koneksi

--- config/koneksi.php
+++ config/koneksi.php
@@ -1,5 +1,5 @@
 <?php
-date_default_timezone_set('Asia/Makassar'); 
+date_default_timezone_set('Asia/Makassar');
 
 // =========================================================
 // --- KONFIGURASI ERROR LOGGING (BARU) ---
@@ -7,11 +7,11 @@ date_default_timezone_set('Asia/Makassar');
 
 // 1. Matikan tampilan error ke layar (Security)
 // Ubah jadi 0 saat sudah online/live. Saat coding boleh 1.
-ini_set('display_errors', 0); 
+ini_set('display_errors', 0);
 ini_set('display_startup_errors', 0);
 
 // 2. Aktifkan pencatatan ke file
-ini_set('log_errors', 1); 
+ini_set('log_errors', 1);
 
 // 3. Tentukan lokasi file log
 // File ini akan otomatis dibuat di folder 'logs'
@@ -23,21 +23,22 @@ ini_set('error_log', $log_folder . '/error_log.txt');
 
 // =========================================================
 $host = 'localhost';
-$db_name = 'smkx8972_pkl';
-$username = 'smkx8972_pkl';
-$password = 'Curseyoubayle123_'; 
+$db_name = 'db_pkl';
+$username = 'root';
+$password = '';
 
 try {
     $pdo = new PDO("mysql:host=$host;dbname=$db_name", $username, $password);
     $pdo->setAttribute(PDO::ATTR_ERRMODE, PDO::ERRMODE_EXCEPTION);
-    
-} catch(PDOException $e) {
+
+} catch (PDOException $e) {
     die("ERROR: Tidak bisa terhubung. " . $e->getMessage());
 }
 
 
 
-function get_settings($pdo) {
+function get_settings($pdo)
+{
     try {
         $stmt = $pdo->query("SELECT key_setting, value_setting FROM pengaturan");
         $settings = [];
@@ -46,7 +47,7 @@ function get_settings($pdo) {
         }
         return $settings;
     } catch (PDOException $e) {
-        return ['grading_start_date' => '2025-01-01']; 
+        return ['grading_start_date' => '2025-01-01'];
     }
 }
 
